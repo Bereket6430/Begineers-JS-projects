ab02f589aceec2054ac429663c398aca04c4bd57
Bereket6430 <beksecured1@gmail.com>
2024-03-11 12:27:43 -0400

    First Commit

---
+++ text typing animation/index.js
@@ -0,0 +1,11 @@
+// Check out below repository for more information about this Typed.js 
+// (Github repo-->Readme.md-->Customization)
+
+// https://github.com/mattboldt/typed.js/
+
+var typed = new Typed(".text-type",{
+    strings: ["Web Developer","Programmer","Android Developer","Designer"],
+    typeSpeed: 200,
+    backSpeed: 200,
+    loop : true
+})
